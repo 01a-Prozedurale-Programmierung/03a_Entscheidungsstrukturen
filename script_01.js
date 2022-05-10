37c1d6ec5bc37fabc129f5f5b9de80116b2d89f2
Peter <schmidt.multimedia@gmail.com>
2022-05-10 14:08:11 +0200

    IF / TINA

--- script_01.js
+++ script_01.js
@@ -3,9 +3,31 @@
 // Entscheidungsstrukturen | control flow
 
 // Deklaration + Assigment
-const ageJohn = 30;
+const ageJohn = 20;
 const ageMark = 30;
+
+console.log(ageJohn);
+console.log(ageMark);
 
 // Deklaration
 let isJohnOlder, isJohnEqual;
+
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+
+/************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+
+// if (true)
+// if (false)
+// if(isJohnOlder)
+if (ageJohn > ageMark)
+{
+    console.log("John ist älter.");
+}
+
 
